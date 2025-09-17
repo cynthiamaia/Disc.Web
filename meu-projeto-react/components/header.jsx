480c4dcaabf8c119c1be49c6cb0101535e616c7f
Cynthia <moreira@Cynthias-MacBook-Air.local>
2025-09-17 09:29:52 -0300

    componente card

--- meu-projeto-react/components/header.jsx
+++ meu-projeto-react/components/header.jsx
@@ -1,14 +1,14 @@
-function Header() {
+export default function Header() {
    return (
-     <header style={styles.header}>
+     <header className="header">
        <h1>Meu Projeto React</h1>
      </header>
    );
  }
-  const styles = {
-   header: {
-     backgroundColor: "#6200ea", //Define a cor de fundo do cabeçalho
-     color: "white", //Define a cor do texto dentro do cabeçalho como branca.
-   },
- };
-  export default Header;
+  //const styles = {
+   //header: {
+    // backgroundColor: "#6200ea", //Define a cor de fundo do cabeçalho
+    // color: "white", //Define a cor do texto dentro do cabeçalho como branca.
+   //},
+ //};
+  //export default Header;
--- meu-projeto-react/src/App.jsx
+++ meu-projeto-react/src/App.jsx
@@ -1,13 +1,16 @@
 import Button from "../components/button"
 import Header from "../components/header"
 import Footer from "../components/footer"
+import Card from "../components/card"
+import "./App.css"
 function App() {
-
+  const handleClick = () => alert("voce clicou no botao")
   return (
     <>
     <Header />
      <main style={{ padding: "1rem" }}>
-       <Button label="Clique aqui" onClick={handleClick} />
+      <Card title="Bem vindo!" content="Este é um componente de card simples em react"></Card>
+      <Button label="Clique aqui" onClick={handleClick} />
      </main>
      <Footer></Footer>
     </>
