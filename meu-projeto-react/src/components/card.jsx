ee31c434963ab05b4638d8348b82f5388b340017
Cynthia <moreira@Cynthias-MacBook-Air.local>
2025-09-24 07:57:43 -0300

    updates

--- meu-projeto-react/src/components/card.jsx
+++ meu-projeto-react/src/components/card.jsx
@@ -1,6 +1,6 @@
 function Card({title, content}){
     return (
-        <div style={styles.card}>
+        <div className="card">
             <h2>{title}</h2>
             <p>{content}</p>
         </div>
--- meu-projeto-react/src/components/footer.jsx
+++ meu-projeto-react/src/components/footer.jsx
@@ -1,15 +1,15 @@
 function Footer() { //indica a seção de rodapé da página 
    return (
-     <footer style={styles.footer}>
+     <footer className="rodape">
        <p> &copy; {new Date().getFullYear()}Todos os direitos reservados</p>
      </footer>
    );
  }
-  const styles = {
-   footer: {
-     backgroundColor: "#6200ea", //Define a cor de fundo do rodapé.
-   },
- };
+ // const styles = {
+  // footer: {
+   //  backgroundColor: "#6200ea", //Define a cor de fundo do rodapé.
+  // },
+ //};
   export default Footer;
 //&copy -> representa o simbolo de copyright
 //new date - criar a data atual
