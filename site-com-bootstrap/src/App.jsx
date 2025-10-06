9b8fdeca05a41f66a19fe822d88c97bccb731863
Cynthia <moreira@Cynthias-MacBook-Air.local>
2025-10-06 07:54:54 -0300

    updates bootstrap

--- site-com-bootstrap/src/App.jsx
+++ site-com-bootstrap/src/App.jsx
@@ -3,12 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import Alert from 'react-bootstrap/Alert';
 import Header from './components/header';
+import Footer from './components/footer'
 function App() {
   return (
     <div>
       <Header></Header>
-      <Alert variant="success">Tudo certo!</Alert>
-      <Button variant="primary">Clique aqui</Button>
+      {/*<Alert variant="success">Tudo certo!</Alert> */}
+      {/*<Button variant="primary">Clique aqui</Button>*/}
+      <Footer/>
     </div>
   )
 }
