1c94597597ab37b0ef52e1d9d39fbb7cf2e3f594
Cynthia <moreira@Cynthias-MacBook-Air.local>
2025-10-06 08:02:01 -0300

    new updates bootstrap

--- site-com-bootstrap/src/components/maincontent.jsx
+++ site-com-bootstrap/src/components/maincontent.jsx
@@ -1,4 +1,8 @@
 import { Container, Row, Col, Card} from 'react-bootstrap'
 function MainContent(){
-    return 
-}
+    return (
+        <Container>
+            <h1> Bem vindo ao meu Site.</h1>
+        </Container>
+    )
+} export default MainContent;
