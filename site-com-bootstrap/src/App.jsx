f586b75f944d58281f586efa537660897852a9ab
Cynthia <moreira@Cynthias-MacBook-Air.local>
2025-10-06 09:32:00 -0300

    upgrades

--- site-com-bootstrap/src/App.jsx
+++ site-com-bootstrap/src/App.jsx
@@ -4,12 +4,16 @@ import Button from 'react-bootstrap/Button';
 import Alert from 'react-bootstrap/Alert';
 import Header from './components/header';
 import Footer from './components/footer'
+import MainContent from './components/maincontent';
 function App() {
   return (
-    <div>
+    <div className="d-flex flex-column min-vh-100 w-100">
       <Header></Header>
       {/*<Alert variant="success">Tudo certo!</Alert> */}
       {/*<Button variant="primary">Clique aqui</Button>*/}
+      <main className="flex-grow-1 w-100">
+        <MainContent></MainContent>
+      </main>
       <Footer/>
     </div>
   )
--- site-com-bootstrap/src/components/maincontent.jsx
+++ site-com-bootstrap/src/components/maincontent.jsx
@@ -1,8 +1,34 @@
 import { Container, Row, Col, Card} from 'react-bootstrap'
 function MainContent(){
     return (
-        <Container>
-            <h1> Bem vindo ao meu Site.</h1>
+        <Container className="my-5">
+            <h1 className="text-center mb-4"> Bem vindo ao meu Site</h1>
+            <Row>
+                <Col md={4}>
+                    <Card>
+                        <Card.Body>
+                           <Card.Title> Servico 1</Card.Title> 
+                           <Card.Text> Descricao breve do servico oferecido</Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col md={4}>
+                <Card>
+                    <Card.Body>
+                        <Card.Title>Servico 2</Card.Title>
+                        <Card.Text>Outro servico destacado</Card.Text>
+                    </Card.Body>
+                </Card>
+                </Col>
+                <Col md={4}>
+                <Card>
+                    <Card.Body>
+                    <Card.Title>Servico 3</Card.Title>
+                    <Card.Text>Outro servico</Card.Text>
+                    </Card.Body>
+                </Card>
+                </Col>
+            </Row>
         </Container>
     )
 } export default MainContent;
