22a4787770f36b72a12d10fe8844326b4690af13
Cynthia <moreira@Cynthias-MacBook-Air.local>
2025-10-20 13:56:26 -0300

    autenticacao

--- site-com-bootstrap/src/App.jsx
+++ site-com-bootstrap/src/App.jsx
@@ -5,18 +5,27 @@ import Alert from 'react-bootstrap/Alert';
 import Header from './components/header';
 import Footer from './components/footer'
 import MainContent from './components/maincontent';
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Register from './components/register';
 function App() {
   return (
+    <Router>
     <div className="d-flex flex-column min-vh-100 w-100">
       <Header></Header>
-      {/*<Alert variant="success">Tudo certo!</Alert> */}
-      {/*<Button variant="primary">Clique aqui</Button>*/}
+      
       <main className="flex-grow-1 w-100">
         <MainContent></MainContent>
       </main>
+      <Routes>
+        <Route path="/" element={<MainContent/>} />
+        <Route path="/register" elment={<Register/>} />
+      </Routes>
       <Footer/>
     </div>
+    </Router>
   )
 }
 
 export default App
+      {/*<Alert variant="success">Tudo certo!</Alert> */}
+      {/*<Button variant="primary">Clique aqui</Button>*/}
