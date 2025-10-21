4277ce00ba685ae906a2b3f452b3fe9d8de744ea
Cynthia <moreira@Cynthias-MacBook-Air.local>
2025-10-21 14:18:11 -0300

    firebase

--- site-com-bootstrap/src/App.jsx
+++ site-com-bootstrap/src/App.jsx
@@ -5,27 +5,25 @@ import Alert from 'react-bootstrap/Alert';
 import Header from './components/header';
 import Footer from './components/footer'
 import MainContent from './components/maincontent';
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Register from './components/register';
 function App() {
   return (
     <Router>
     <div className="d-flex flex-column min-vh-100 w-100">
-      <Header></Header>
-      
+      <Header></Header>     
       <main className="flex-grow-1 w-100">
-        <MainContent></MainContent>
-      </main>
       <Routes>
-        <Route path="/" element={<MainContent/>} />
-        <Route path="/register" elment={<Register/>} />
-      </Routes>
+        <Route path="/" element={<MainContent/>}/>
+        <Route path="/register" element={<Register/>}/> {}
+      </Routes>  
+      </main> 
       <Footer/>
     </div>
     </Router>
   )
 }
 
-export default App
-      {/*<Alert variant="success">Tudo certo!</Alert> */}
-      {/*<Button variant="primary">Clique aqui</Button>*/}
+export default App;
+     // {/*<Alert variant="success">Tudo certo!</Alert> */}
+     // {/*<Button variant="primary">Clique aqui</Button>*/}
--- site-com-bootstrap/src/components/register.jsx
+++ site-com-bootstrap/src/components/register.jsx
@@ -1,52 +1,52 @@
-import {useState} from "react";
-import { createUserWithEmailandPassword, updateProfile } from "firebase/auth";
-import {auth} from "../firebase";
+import  { useState } from "react";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth } from "../firebase";
 function Register() {
     //inicializa o estado com string vazia
-    const [nome, setNome] = useState("")
-    const [email, setEmail] = useState("")
-    const [senha, setSenha] = useState("")
-    const [erro, setErro] = useState("")
+    const [nome, setNome] = useState("");
+    const [email, setEmail] = useState("");
+    const [senha, setSenha] = useState("");
+    const [erro, setErro] = useState("");
 
-//impede o carregamento, sem ela, a pagina recarregaria
-const handleRegister = async (e) => {
-e.preventDefault();
-//await -> faz a funcao esperar ate o firebase confirmar a criacao do usuario
-try {
-    const userCredential = await createUserWithEmailandPassword(auth, email, senha)
-} catch (error){
-    setErro(error.message);
-}
-}
+    //impede o carregamento, sem ela, a pagina recarregaria
+    const handleRegister = async (e) => {
+        e.preventDefault();
+        //await -> faz a funcao esperar ate o firebase confirmar a criacao do usuario
+        try {
+            const userCredential = await createUserWithEmailAndPassword(auth, email, senha);
+        } catch (error){
+            setErro(error.message);
+        }
+        };
 
-return (
-    <div>
-       <h2>Cadastro</h2> 
-       <form onSubmit={handleRegister}>
-        <input
-            type="text"
-            placeholder="Nome"
-            value={nome}
-            onChange={(e) => setNome(e.target.value)}
-            required
-        />
-         <input
-            type="text"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-        />
-         <input
-            type="text"
-            placeholder="Senha"
-            value={senha}
-            onChange={(e) => setSenha(e.target.value)}
-            required
-        />
-        <button type="submit">Cadastrar</button>
-       </form>
-    </div>
-)
-}
+    return (
+        <div>
+        <h2>Cadastro</h2> 
+        <form onSubmit={handleRegister}>
+            <input
+                type="text"
+                placeholder="Nome"
+                value={nome}
+                onChange={(e) => setNome(e.target.value)}
+                required
+            />
+            <input
+                type="text"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+            />
+            <input
+                type="text"
+                placeholder="Senha"
+                value={senha}
+                onChange={(e) => setSenha(e.target.value)}
+                required
+            />
+            <button type="submit">Cadastrar</button>
+        </form>
+        </div>
+    )
+    }
 export default Register;
